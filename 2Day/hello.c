55e5e298cdbffabf68fe8583e35b15f71236c319
nickeloid99 <159232262+nickeloid99@users.noreply.github.com>
2024-02-07 15:56:20 +0900

    Create hello.c

---
+++ 2Day/hello.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void myPrint() 
+{
+    printf("hello print\n");
+
+}
+int add()
+{
+    int a = 100;
+    int b = 200;
+    printf("%d\n", a+b);
+}
+int main()
+{
+    myPrint();
+    return 0;
+}
